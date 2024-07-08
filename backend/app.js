ae1ba673c717780d6a0aed8bc78e80032c18848f
jdhote <janhavidhote28@gmail.com>
2024-07-08 22:48:26 +0530

    add app.js to backend

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("hi");
+})
+
+//Setting up the server at port 8080
+const port =8080;
+app.listen(port,()=>{
+    console.log(`server is listening to port ${port}`);
+});
