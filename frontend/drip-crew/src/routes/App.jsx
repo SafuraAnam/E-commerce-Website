b9ba91e8a930f3c395919c0049751eff5b957a2c
SafuraAnam <safu.sheikh2003@gmail.com>
2024-07-09 08:42:59 +0530

    routes and index created

---
+++ frontend/drip-crew/src/routes/App.jsx
@@ -0,0 +1,12 @@
+import Header from "../Components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+}
+
+export default App;
