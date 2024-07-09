ca5974386ea248d056e7825fe21209b89a67861c
jdhote <janhavidhote28@gmail.com>
2024-07-09 16:53:39 +0530

    changes made by j

---
+++ backend/init/data.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const sampleUsers = [
+ 
+    { name: 'John Doe', avatar:"../../assets/add-friend-icon.svg"},
+    { name: 'Jane Smith',avatar:"../../assets/add-friend-icon.svg"},
+    { name: 'Alice Johnson', avatar: "../../assets/add-friend-icon.svg"},
+     
+    { name: 'John Doe', avatar:"../../assets/add-friend-icon.svg"},
+    { name: 'Jane Smith',avatar:"../../assets/add-friend-icon.svg"},
+    { name: 'Alice Johnson', avatar: "../../assets/add-friend-icon.svg"},  
+    
+    { name: 'John Doe', avatar:"../../assets/add-friend-icon.svg"},
+    { name: 'Jane Smith',avatar:"../../assets/add-friend-icon.svg"},
+    { name: 'Alice Johnson', avatar: "../../assets/add-friend-icon.svg"},
+
+   
+];
+module.exports = { userData: sampleUsers };
--- frontend/drip-crew/src/routes/App.jsx
+++ frontend/drip-crew/src/routes/App.jsx
@@ -1,11 +1,13 @@
 import Header from "../Components/Header";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Chats from "../Components/Chats/Chats";
 
 function App() {
   return (
     <>
       <Header></Header>
+      
     </>
   );
 }
